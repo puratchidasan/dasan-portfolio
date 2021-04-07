eafeddfebb0db8c699a6bc99a97e6ee97e37d372
Puratchidasan Munusami <puratchidasan@gmail.com>
2021-04-07 19:15:52 +0200

    Added Time Angular Component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { IntervalObservable } from "rxjs/observable/IntervalObservable";
+import 'rxjs/add/operator/share';
+import * as moment from 'moment';
+
 
 declare const $: any;
 declare interface RouteInfo {
@@ -25,11 +31,34 @@ export const ROUTES: RouteInfo[] = [
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
+  name = 'Angular 4';
+  date:String;
+  hours:any;
+  minutes:any;
+  seconds:any;
+  currentLocale: any;
 
-  constructor() { }
+  isTwelveHrFormat:false;
+  test:any;
+
+  constructor(){
+    //   let now = moment(); // add this 2 of 4
+      //navigator.language || navigator.userLanguage; 
+ 
+  //var test = moment('2016-01-24 14:23:45');
+ 
+        //ja-JP;
+  //de-DE
+   
+   }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
+    setInterval(() =>{
+      const currentDate = new Date();
+      this.date = currentDate.toLocaleTimeString();
+      //console.log("The current Date: "+ this.date);
+       }, 1000);
   }
   isMobileMenu() {
       if ($(window).width() > 991) {
@@ -38,3 +67,11 @@ export class SidebarComponent implements OnInit {
       return true;
   };
 }
+ // IntervalObservable.create(1000).map(tick => new Date()).share()   
+    // .subscribe( res =>{
+     
+    //   this.date = res;
+    //    this.hours =  this.date.getHours();
+    //    this.minutes = this.date.getMinutes();
+    //    this.seconds = this.date.getSeconds();
+    // }) 
