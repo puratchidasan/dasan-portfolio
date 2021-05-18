f43ec072ab0b4efbe8104cd714ee78b8c4dd83ae
Puratchidasan Munusami <puratchidasan@gmail.com>
2021-05-18 22:13:44 +0200

    Updated the code for correct REST link

--- src/app/table-list/table-list.service.ts
+++ src/app/table-list/table-list.service.ts
@@ -11,11 +11,11 @@ constructor(private http: HttpClient) { }
   getElements(): Observable<Element[]> {
     //return this.http.get<Element[]>('https://student-services.azurewebsites.net/elements',{'headers': headers });
     return this.http.get<Element[]>('http://localhost:8083/student-service/elements');
-    return this.http.get<Element[]>('http://140.238.89.162:8080/student-jpa');
+    //return this.http.get<Element[]>('http://140.238.89.162:8080/student-jpa');
   }
 
   getStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>('http://140.238.89.162:8080/student-jpa');
+    return this.http.get<Student[]>('http://140.238.89.162:8088/student-jpa');
   }
 }
 export interface Element {
