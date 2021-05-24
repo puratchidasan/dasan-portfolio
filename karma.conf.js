1e3ff80c2df5e67fc838b04a2d92ad95226c86fb
Puratchidasan Munusami <puratchidasan@gmail.com>
2021-05-24 20:12:29 +0200

    Sonar code review updated

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -11,43 +8,20 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-sonarqube-unit-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('karma-spec-reporter') // Add this
     ],
-    sonarQubeUnitReporter: {
-      sonarQubeVersion: 'LATEST',
-      outputFile: 'reports/ut_report.xml',
-      overrideTestDescription: true,
-      testPaths: ['./src/app'],
-      testFilePattern: '.spec.ts',
-      useBrowserName: false
-    },
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    files: [
-      
-    ],
-    preprocessors: {
-      'src/**/*.js':   ['coverage'],
-      'test/**/*.js':   ['coverage']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul', 'sonarqubeUnit']
-              : ['progress', 'kjhtml'],
+    reporters: ['spec', 'kjhtml'], // Update progress to spec
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
